70f1e3a6e16e9ebad6e2e7b3a784577f9468d72d
fullstacknyc <123508794+4ur4-1@users.noreply.github.com>
2024-10-01 05:38:04 -0400

    feat: add Search bar

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,11 +1,28 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import Header from './Header.js';
+import Search from './Search.js';
 import './Container.css';
 
 function Container() {
+  const [emojiData, setEmojiData] = useState([]);
+  const [newEmojiData, setNewEmojiData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
+    setEmojiData(data);
+  }, []);
+
+  const onChange = useCallback(
+    val => {
+      setSearchQuery(val.toLowerCase());
+    },
+    [setNewEmojiData, setSearchQuery, emojiData]
+  );
+
   return (
     <div className="container">
       <Header />
+      <Search onChange={onChange} />
     </div>
   );
 }
